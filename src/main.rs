fd01d0c9eebfb1275d7791670b1cf2303b3a6c34
williamtuominiemi <william.m.tuominiemi@gmail.com>
2025-09-16 00:19:13 +0300

    list of pressed buttons

--- src/main.rs
+++ src/main.rs
@@ -38,15 +38,25 @@ fn ds_listen<T: UsbContext>(device: Device<T>) -> Result<(), Box<dyn std::error:
 
     let mut buf = [0u8; 64];
     
+    let mut ds_layout: Vec<(&str, bool)> = vec![
+        ("cross", false), ("square", false), ("triangle", false), ("circle", false),
+        ("down", false), ("left", false), ("up", false), ("right", false),
+        ("L2", false), ("L1", false), ("R2", false), ("R1", false),
+        ("Share", false), ("Options", false), ("L_joystick", false), ("R_joystick", false)
+    ];
+    
     loop {
         match ds_handle.read_interrupt(ds_endpoint, &mut buf, Duration::from_millis(1000)) {
             Ok(len) => {
                 //println!("Received {} bytes: {:02x?}", len, &buf[..len]);
                 let button: &u8 = &buf[..len][5];
+                let special: &u8 = &buf[..len][6];
 
-                //println!("{:?}", button);
-                ds_button(button);            
-                                    
+
+                println!("{:?}", ds_layout);
+
+                ds_button(button, &mut ds_layout);            
+                ds_special(special, &mut ds_layout);                    
             }
             Err(rusb::Error::Timeout) => {
                 continue;
@@ -61,18 +71,32 @@ fn ds_listen<T: UsbContext>(device: Device<T>) -> Result<(), Box<dyn std::error:
     Ok(())
 }
 
-fn ds_button(button: &u8) {
+fn ds_button(button: &u8, ds_layout: &mut Vec<(&str, bool)>) {
     match button {
         // Action buttons
-        40 => println!("X pressed"),
-        24 => println!("Square pressed"),
-        136 => println!("Triangle pressed"),
-        72 => println!("Circle pressed"),
+        40 => ds_layout[0].1 = true, // Cross
+        24 =>  ds_layout[1].1 = true, // Square
+        136 => ds_layout[2].1 = true, // Triangle
+        72 => ds_layout[3].1 = true, // Circle
         // Dpad
-        00 => println!("Up pressed"),
-        02 => println!("Right pressed"),
-        04 => println!("Down pressed"),
-        06 => println!("Left pressed"),
+        04 => ds_layout[4].1 = true, // Down
+        06 => ds_layout[5].1 = true, // Left
+        00 => ds_layout[6].1 = true, // Up
+        02 => ds_layout[7].1 = true, // Right
+        _ => (),
+    }
+}
+
+fn ds_special(input: &u8, ds_layout: &mut Vec<(&str, bool)>) {
+    match input {
+        0x04 => ds_layout[8].1 = true, // L2
+        0x01 => ds_layout[9].1 = true, // L1
+        0x08 => ds_layout[10].1 = true, // R2
+        0x02 => ds_layout[11].1 = true, // R1
+        0x10 => ds_layout[12].1 = true, // Share
+        0x20 => ds_layout[13].1 = true, // Options
+        0x40 => ds_layout[14].1 = true, // Left joystick
+        0x80 => ds_layout[15].1 = true, // Right joystick
         _ => (),
     }
 }
