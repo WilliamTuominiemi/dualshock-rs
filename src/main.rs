cff2cde4f9e88f031ced8449f6467e1b2d03aad5
williamtuominiemi <william.m.tuominiemi@gmail.com>
2025-09-17 22:00:54 +0300

    better control over inputs, fixed byte contamination bug

--- src/main.rs
+++ src/main.rs
@@ -53,36 +53,39 @@ fn ds_listen<T: UsbContext>(device: Device<T>) -> Result<(), Box<dyn std::error:
     let mut buf = [0u8; 64];
     
     let mut ds_layout = vec![
-        // Action buttons
-        Input {name: String::from("cross"), code: 40, pressed: false},
-        Input {name: String::from("square"), code: 24, pressed: false},
-        Input {name: String::from("triangle"), code: 136, pressed: false},
-        Input {name: String::from("circle"), code: 72, pressed: false},
-        // D-pad
-        Input {name: String::from("down"), code: 04, pressed: false},
-        Input {name: String::from("left"), code: 06, pressed: false},
-        Input {name: String::from("up"), code: 00, pressed: false},
-        Input {name: String::from("right"), code: 02, pressed: false},
-        // Triggers
-        Input {name: String::from("L2"), code: 0x04, pressed: false},
-        Input {name: String::from("L1"), code: 0x01, pressed: false},
-        Input {name: String::from("R2"), code: 0x08, pressed: false},
-        Input {name: String::from("R1"), code: 0x02, pressed: false},
-        // Flat buttons 
-        Input {name: String::from("options"), code: 0x10, pressed: false},
-        Input {name: String::from("share"), code: 0x20, pressed: false},
-        // Joysticks
-        Input {name: String::from("left_joystick"), code: 0x40, pressed: false},
-        Input {name: String::from("right_joystick"), code: 0x80, pressed: false},
+        // Byte at position 5
+        vec![
+            // Action buttons
+            Input {name: String::from("cross"), code: 40, pressed: false},
+            Input {name: String::from("square"), code: 24, pressed: false},
+            Input {name: String::from("triangle"), code: 136, pressed: false},
+            Input {name: String::from("circle"), code: 72, pressed: false},
+            // D-pad
+            Input {name: String::from("down"), code: 04, pressed: false},
+            Input {name: String::from("left"), code: 06, pressed: false},
+            Input {name: String::from("up"), code: 00, pressed: false},
+            Input {name: String::from("right"), code: 02, pressed: false},
+        ],
+        // Byte at position 6
+        vec! [
+            // Triggers
+            Input {name: String::from("L2"), code: 0x04, pressed: false},
+            Input {name: String::from("L1"), code: 0x01, pressed: false},
+            Input {name: String::from("R2"), code: 0x08, pressed: false},
+            Input {name: String::from("R1"), code: 0x02, pressed: false},
+            // Flat buttons 
+            Input {name: String::from("options"), code: 0x10, pressed: false},
+            Input {name: String::from("share"), code: 0x20, pressed: false},
+            // Joysticks
+            Input {name: String::from("left_joystick"), code: 0x40, pressed: false},
+            Input {name: String::from("right_joystick"), code: 0x80, pressed: false},
+        ]
     ];
     
     loop {
         match ds_handle.read_interrupt(ds_endpoint, &mut buf, Duration::from_millis(1000)) {
             Ok(len) => {
-                let button: &u8 = &buf[..len][5];
-                let special: &u8 = &buf[..len][6];
-
-                ds_button(button, special, &mut ds_layout);            
+                ds_button(&buf[..len], &mut ds_layout);            
             }
             Err(rusb::Error::Timeout) => {
                 continue;
@@ -97,12 +100,19 @@ fn ds_listen<T: UsbContext>(device: Device<T>) -> Result<(), Box<dyn std::error:
     Ok(())
 }
 
-fn ds_button(button: &u8, special: &u8, ds_layout: &mut Vec<Input>) {
-    for input in ds_layout.iter_mut() {
-        if !input.pressed && (*button == input.code || *special == input.code) {
+fn ds_button(data: &[u8], ds_layout: &mut Vec<Vec<Input>>) {
+    for input in &mut ds_layout[0] {
+        if !input.pressed && data[5] == input.code {
             input.press();
             println!("{:?}", input);
         }
-    }
+    } 
+
+    for input in &mut ds_layout[1] {
+        if !input.pressed && data[6] == input.code {
+            input.press();
+            println!("{:?}", input);
+        }
+    }  
 }
 
